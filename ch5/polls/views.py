6eff05a48a25b9617c97a3242654fa3cf96dd1e9
Hong3eee <jk4986@naver.com>
2017-08-08 04:27:43 +0900

    study finished django basic book, now I'm going to study next book

--- ch5/polls/views.py
+++ ch5/polls/views.py
@@ -2,18 +2,30 @@ from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponseRedirect , HttpResponse
 from django.core.urlresolvers import reverse
 from polls.models import Question,Choice
+from django.views.generic import ListView , DetailView
+
+import logging
+logger = logging.getLogger(__name__)
+
+class IndexView(ListView):
+		template_name = 'polls/index.html'
+		context_object_name = 'latest_question_list'
+
+		def get_queryset(self):
+				return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(DetailView):
+		model = Question
+		template_name = 'polls/detail.html'
+
+
+class ResultsView(DetailView):
+		model = Question
+		template_name = 'polls/results.html'
 
 # Create your views here.
-def index(req):
-		latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
-		context = {'latest_question_list': latest_question_list}
-		return render(req ,'polls/index.html',context)
-
-def detail(req,question_id):
-		question = get_object_or_404(Question, pk=question_id)
-		return render(req, 'polls/detail.html',{'question':question})
-
 def vote(req,question_id):
+		logger.debug("vote().question_id: %s" % question_id)
 		p = get_object_or_404(Question, pk = question_id)
 		try:
 				selected_choice = p.choice_set.get(pk=req.POST['choice'])
@@ -27,6 +39,3 @@ def vote(req,question_id):
 				selected_choice.save()
 				return HttpResponseRedirect(reverse('polls:results',args=(p.id,)))
 
-def results(req,question_id):
-		question = get_object_or_404(Question,pk=question_id)
-		return render(req,'polls/results.html',{'question':question})
